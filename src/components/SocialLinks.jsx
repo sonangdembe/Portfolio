8cd3e72f5c237230892f83c1b583767fa8425ede
sona angdembe <asonaxaviersm1@gmail.com>
2024-08-20 14:26:28 +0545

    icon completed

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -1,18 +1,81 @@
-import React from 'react'
+import React, {useNavigate}from 'react'
 import { FaGithub, FaLinkedin } from 'react-icons/fa'
-import { HiOutLineMail } from 'react-icons/hi'
 import { BsFillPersonLinesFill } from 'react-icons/bs'
-
+import { HiOutlineMail } from "react-icons/hi";
+import { PiReadCvLogoFill } from "react-icons/pi";
 
 const SocialLinks = () => {
+
+    const Links = [{
+        id:1,
+        child:(
+            <>
+           <span className='ml-8'>LinkedIn</span>
+           <FaLinkedin size={20}  className='ml-3'/>
+            </>
+        ),
+        href:'https://www.linkedin.com/in/sona-angdembe/',
+        style : 'rounded-tr-md'
+    },
+    {
+        id:2,
+        child:(
+            <>
+               <span className='ml-11'>Github</span>
+            <FaGithub size={20} className='ml-3' />
+            
+            </>
+        ),
+        href:'https://github.com/sonangdembe',
+     
+    },
+    {
+        id:3,
+        child:(
+            <>
+            <span className='ml-10'>Email </span>
+           <HiOutlineMail size={20} className='ml-6' />
+            
+            </> 
+        ),
+        href:'https://mail.google.com/mail',
+       
+    },
+    {
+        id:4,
+        child:(
+            <>
+             <span className='ml-10'>CV </span>
+         <PiReadCvLogoFill  size={20} className='ml-11' />
+            </>
+        ),
+        href:'/CV.pdf',
+         style : 'rounded-br-md',
+         download:true,
+    }
+]
+    
+
     return (
         <>
         <div className='flex flex-col top-[35%] left-0 fixed'>
             <ul>
-               <li className='flex justify-between items-center w-40 h-1/4 px-4 bg-black'>
-                <a href='' />
-                Linkedin <FaLinkedin/>
-                </li>
+
+            {Links.map (({id, child, href, style,download})=>(
+            <li 
+             key={id} 
+             className={'flex justify-between items-center w-40 h-14 px-4 ml-[-110px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500' + ' ' + style} >
+          <a 
+          href={href}
+          className='flex items-center w-full text-white'
+          download = {download}
+          target='_blank'
+          rel="noreferrer"
+          >
+            {child}
+          </a>
+        </li>
+        ))}
             </ul>
         </div>
         </>
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -5,24 +5,24 @@ import Button from '../components/Button';
 import SocialLinks from '../components/SocialLinks';
 
 const Home = () => {
-
   return (
     <>
-    <Navbar/>
-    <div name='home' className='text-white h-screen w-full bg-gradient-to-b from-black via-black to-gray-800'>
+      <Navbar />
+      <div name='home' className='text-white h-screen w-full bg-gradient-to-b from-black via-black to-gray-800'>
+        <SocialLinks />
         <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
-          <div className='flex flex-col justify-center h-full ml-20'> 
-            <h2 className='text-4xl sm:text-7xl font-bold ml-20'>I'm a React <br/>Developer</h2>
-            <p className='text-gray-500 py-4 max-w-md ml-20'>
+          <div className='flex flex-col justify-center h-full md:ml-20'>
+            <h2 className='text-4xl sm:text-7xl font-bold md:ml-20'>I'm a React <br />Developer</h2>
+            <p className='text-gray-500 py-4 max-w-md md:ml-20'>
               I have learned about React through bootcamps and later I joined as a student of React in DigitalPathsala 
               and currently exploring more about React.
             </p>
             <div>
               <Button
-                className=" group border text-white w-fit px-6 py-3 ml-20 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer">
+                className="group border text-white w-fit px-6 py-3 md:ml-20 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer">
                 portfolio
                 <span className='group-hover:rotate-90 duration-300'>
-                  <MdArrowRightAlt className="ml-2" size={25}/>
+                  <MdArrowRightAlt className="ml-2" size={25} />
                 </span>
               </Button>
             </div>
@@ -31,12 +31,11 @@ const Home = () => {
             <img 
               src='./images/pic.jpg' 
               alt='my profile' 
-              className='rounded-2xl w-80 h-80 ' 
-              />
+              className='rounded-2xl w-80 h-80 object-cover' 
+            />
           </div>
         </div>
       </div>
-
     </>
   );
 };
